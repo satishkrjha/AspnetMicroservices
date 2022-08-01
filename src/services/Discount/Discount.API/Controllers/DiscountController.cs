dbd50cd2a0cbe050989f18657335ff65ccbf7e0b
SATISH JHA <satishjhasw@gmail.com>
2022-08-01 20:44:50 +0530

    Discount.API

---
+++ src/services/Discount/Discount.API/Controllers/DiscountController.cs
@@ -0,0 +1,50 @@
+﻿using Discount.API.Entities;
+using Discount.API.Repositories;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using System.Net;
+
+namespace Discount.API.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class DiscountController : ControllerBase
+    {
+        private readonly IDiscountRepository _discountRepository;
+
+        public DiscountController(IDiscountRepository discountRepository)
+        {
+            _discountRepository = discountRepository;
+        }
+
+        [HttpGet("{ProductName}", Name = "GetDiscount")]
+        [ProducesResponseType(typeof(Coupon), (int)HttpStatusCode.OK)]
+        public async Task<ActionResult<Coupon>> GetDiscount(string ProductName)
+        {
+            var coupon = await _discountRepository.GetDiscount(ProductName);
+            return Ok(coupon);
+        }
+        [HttpPost]
+        [ProducesResponseType(typeof(void), (int)HttpStatusCode.OK)]
+        public async Task<ActionResult<bool>> CreateDiscount(Coupon coupon)
+        {
+            var affected = await _discountRepository.CreateDiscount(coupon);
+            return Ok(affected);
+        }
+        [HttpPut]
+        [ProducesResponseType(typeof(void), (int)HttpStatusCode.OK)]
+        public async Task<ActionResult<bool>> UpdateDiscount(Coupon coupon)
+        {
+            var affected = await _discountRepository.UpdateDiscount(coupon);
+            return Ok(affected);
+        }
+
+        [HttpDelete("{ProductName}", Name = "DeleteDiscount")]
+        [ProducesResponseType(typeof(void), (int)HttpStatusCode.OK)]
+        public async Task<ActionResult<bool>> DeleteDiscount(string ProductName)
+        {
+            var affected = await _discountRepository.DeleteDiscount(ProductName);
+            return Ok(affected);
+        }
+    }
+}
---
+++ src/services/Discount/Discount.API/Extensions/HostExtensions.cs
@@ -0,0 +1,62 @@
+﻿using Npgsql;
+
+namespace Discount.API.Extensions
+{
+    public static class HostExtensions
+    {
+        public static IHost MigrationDatabase<TContext>(this IHost host, int? retry = 0)
+        {
+            using (var scope = host.Services.CreateScope())
+            {
+                var service = scope.ServiceProvider;
+                var logger = service.GetRequiredService<ILogger<TContext>>();
+                var configuration = service.GetRequiredService<IConfiguration>();
+
+                using (var connection = new NpgsqlConnection(configuration.GetValue<string>("DatabaseSetting:ConnectionString")))
+                {
+                    try
+                    {
+                        logger.LogInformation("Migration postrewql database started");
+
+                        string script = "DROP TABLE IF EXISTS coupon";
+
+                        connection.Open();
+                        var commnd = new NpgsqlCommand(script, connection);
+                        commnd.ExecuteNonQuery();
+
+                         script = @"CREATE TABLE IF NOT EXISTS coupon
+                        (
+                            id serial  primary key,
+                            productname character varying(24) ,
+                            description text,
+                            amount integer
+                        )";
+
+                        commnd = new NpgsqlCommand(script, connection);
+                        commnd.ExecuteNonQuery();
+
+                         script = @"INSERT INTO coupon(productname, description, amount)
+	                    VALUES ('APPle Iphone', 'Smart phone', 2000);
+                        INSERT INTO coupon(productname, description, amount)
+	                    VALUES ('Samsung Galaxy', 'Smart phone', 1000)";
+                       
+
+                        commnd = new NpgsqlCommand(script, connection);
+                        commnd.ExecuteNonQuery();
+
+                        logger.LogInformation("Migration postrewql database completed");
+                    }
+                    catch (Exception ex)
+                    {
+                        logger.LogError(ex, "error occured while migrating db");
+                        throw;
+                    }
+                    finally { connection.Close(); }
+                }
+
+            }
+            return host;
+
+        }
+    }
+}
